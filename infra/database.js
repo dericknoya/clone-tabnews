c36bef022ff9902f85c077959c9f119bb275928e
DerickNoya <derick.noya@gmail.com>
2024-02-20 19:15:26 -0300

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: getSSLValues(),
     // se trabalhando com banco local ssl não é suportado portante usar "false" ou simplesmente não declarar o parâmetro "ssl" ou use condição ternária verificando a condição da variável NODE_ENV
     // NODE_ENV: variável de ambiente do node que pode ter os valores development ou production a dependenr do ambiente em que a aplicação esteja rodando.
   });
@@ -34,3 +34,12 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    }
+  }
+  return process.env.NODE_ENV === "development" ? false : true
+}
