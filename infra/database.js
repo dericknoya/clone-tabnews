d8b2096c22214bacdc2f9e43ffbf48777aae62ca
DerickNoya <derick.noya@gmail.com>
2024-04-03 11:24:49 -0300

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,26 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-    // se trabalhando com banco local ssl não é suportado portante usar "false" ou simplesmente não declarar o parâmetro "ssl" ou use condição ternária verificando a condição da variável NODE_ENV
-    // NODE_ENV: variável de ambiente do node que pode ter os valores development ou production a dependenr do ambiente em que a aplicação esteja rodando.
-  });
-  console.log('Credenciais do Postgres:', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-  
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -31,8 +14,22 @@ async function query(queryObject) {
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),    
+  });
+
+  await client.connect();
+  return client;
+}
 export default {
-  query: query,
+  query,
+  getNewClient
 };
 
 function getSSLValues() {
@@ -41,5 +38,5 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     }
   }
-  return process.env.NODE_ENV === "development" ? false : true
+  return process.env.NODE_ENV === "production" ? true : false;
 }
