50cc741983413297e843e305602fc1eda3b84a8f
DerickNoya <derick.noya@gmail.com>
2024-05-08 19:07:33 -0300

    fix  endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,7 +3,17 @@ import { join } from "node:path"
 import database from 'infra/database.js'
 
 export default async function status(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+  
+  let dbClient;
+
+  try {
+  dbClient = await database.getNewClient();
 
   const defaultMigrationOptions = {
     dbClient: dbClient,
@@ -15,7 +25,6 @@ export default async function status(request, response) {
   }
   if (request.method === 'GET') {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions)
-    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
   if (request.method === 'POST') {
@@ -24,14 +33,17 @@ export default async function status(request, response) {
       dryRun: false,
     })
 
-    await dbClient.end();
-    
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
     return response.status(200).json(migratedMigrations);
   }
 
-  return response.status(405).end();
   
+} catch (error) {
+  console.error(error)
+  throw error;
+} finally {
+  await dbClient.end();
+}
 }
